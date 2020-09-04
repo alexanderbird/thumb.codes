17974b6feb08d9589f524d602edfcb9c6af847ed
Alexander Bird <alex@alexanderbird.software>
2020-09-04 11:38:24 -0700

    Various minor improvements (see list in full commit description)
     - search terms with spaces match any order
     - show more results, but style the first 10 darker
     - add instruction text for putting emoji on the clipboard
     - improved styling (minor)
     - default search to 'thumbs' because this app is called thumbs.codes

--- app/app.js
+++ app/app.js
@@ -10,19 +10,27 @@ async function main() {
   let topResult = false;
 
   function onSearchKeyup() {
-    const query = searchInput.value.length <= 1 ? '' : searchInput.value.replace(/ /g, '.*');
+    const defaultQuery = 'thumbs';
+    const query = searchInput.value.length > 0 ? searchInput.value.trim() : defaultQuery;
 
     const bestGuess = results[query.slice(0,-1)] || emoji;
 
-    const result = results[query] || bestGuess.filter(x => x.description.match(query));
+    const queryTerms = query.split(' ');
+    const result = results[query] || bestGuess.filter(x => queryTerms.every(term => x.description.match(term)));
     results[query] = result;
     topResult = result[0];
 
+    const maxResults = 50;
+
     if (topResult) {
       resultElement.innerHTML = `
-        <h2>${result[0].emoji} ${result[0].description}</h2>
-        ${result.slice(1,11).map(({ emoji, description }) => `<div> ${emoji} ${description}</div>`).join('\n')}
-        ${result.length > 11 ? `<div>(and ${result.length - 11} more)</div>` : ''}
+        <div class='top-result'>
+          <h2 class='top-result__result'>${result[0].emoji} ${result[0].description}</h2>
+          <div class='top-result__instructions'>(Enter/Submit to copy)</div>
+        </div>
+
+        ${result.slice(1, 1 + maxResults).map(({ emoji, description }) => `<div> ${emoji} ${description}</div>`).join('\n')}
+        ${result.length > (1 + maxResults) ? `<div>(and ${result.length - 1 - maxResults} more)</div>` : ''}
       `;
     } else {
       resultElement.innerHTML = `No results for ${query}`;
